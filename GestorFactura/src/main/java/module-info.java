5d7a956a3168b89b1d4817f73c553af226a26ca1
Mohamed Amen <amenelbou@gmail.com>
2025-11-14 10:33:37 +0100

    Corrijo ConexionBBDD

--- GestorFactura/src/main/java/module-info.java
+++ GestorFactura/src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module davinci.gestorfactura {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    
     opens davinci.gestorfactura to javafx.fxml;
     exports davinci.gestorfactura;
 }
