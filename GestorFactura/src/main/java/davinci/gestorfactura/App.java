b07a731bbdfc18d1e3488a64351b5110682ae6a0
Stefano <stefano.work19@gmail.com>
2025-11-15 19:06:05 +0100

    modified primary(visual), pom, add estilo

--- GestorFactura/src/main/java/davinci/gestorfactura/App.java
+++ GestorFactura/src/main/java/davinci/gestorfactura/App.java
@@ -18,8 +18,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 696, 494);
         stage.setScene(scene);
+        stage.sizeToScene();
+        stage.setMinWidth(scene.getWidth()+16);
+        stage.setMinHeight(scene.getHeight()+39);
         stage.show();
     }
 
@@ -33,7 +36,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
--- GestorFactura/src/main/java/davinci/gestorfactura/PrimaryController.java
+++ GestorFactura/src/main/java/davinci/gestorfactura/PrimaryController.java
@@ -1,12 +1,45 @@
 package davinci.gestorfactura;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable{
 
+    @FXML
+    private Button button_abrir;
+
+    @FXML
+    private Button button_empre;
+
+    @FXML
+    private Button button_informacion;
+
+    @FXML
+    private Button button_nuevo;
+    
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        cursorHand();
+    }
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    
+    public void cursorHand(){
+        button_abrir.setCursor(Cursor.HAND);
+        button_empre.setCursor(Cursor.HAND);
+        button_informacion.setCursor(Cursor.HAND);
+        button_nuevo.setCursor(Cursor.HAND);
+                
     }
 }
