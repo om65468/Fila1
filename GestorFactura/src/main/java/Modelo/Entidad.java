cf63a99fbcd439b52efaa75ca2b2d8ad38342845
Mohamed Amen <amenelbou@gmail.com>
2025-11-18 10:32:28 +0100

    Elimino Direccion y cambio par de cositas :P

--- GestorFactura/src/main/java/Modelo/Entidad.java
+++ GestorFactura/src/main/java/Modelo/Entidad.java
@@ -5,16 +5,22 @@ public class Entidad {
     private int id;
     private String nombre;
     private String nif;
-    private Direccion direccion;
+    private String calle;
+    private String codPost;
+    private String ciudad;
+    private String telfono;
     private String email;
     private String telefono;
     private int tipo;
-    
-    public Entidad(int id, String nombre, String nif, Direccion direccion, String email, String telefono, int tipo) {
+
+    public Entidad(int id, String nombre, String nif, String calle, String codPost, String ciudad, String telfono, String email, String telefono, int tipo) {
         this.id = id;
         this.nombre = nombre;
         this.nif = nif;
-        this.direccion = direccion;
+        this.calle = calle;
+        this.codPost = codPost;
+        this.ciudad = ciudad;
+        this.telfono = telfono;
         this.email = email;
         this.telefono = telefono;
         this.tipo = tipo;
@@ -24,56 +30,81 @@ public class Entidad {
         return id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getNif() {
-        return nif;
-    }
-
-    public Direccion getDireccion() {
-        return direccion;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public int getTipo(){
-        return tipo;
-    }
-    
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
     }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    public String getNif() {
+        return nif;
+    }
+
     public void setNif(String nif) {
         this.nif = nif;
     }
 
-    public void setDireccion(Direccion direccion) {
-        this.direccion = direccion;
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getCodPost() {
+        return codPost;
+    }
+
+    public void setCodPost(String codPost) {
+        this.codPost = codPost;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getTelfono() {
+        return telfono;
+    }
+
+    public void setTelfono(String telfono) {
+        this.telfono = telfono;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
     public void setTelefono(String telefono) {
         this.telefono = telefono;
     }
 
-    public void settipo(int tipo){
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
         this.tipo = tipo;
     }
-       
+    
+        
 }
