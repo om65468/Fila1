f30aef91f8a374bdb947d1ab0ad773d252704ec6
Stefano <stefano.work19@gmail.com>
2025-11-25 21:34:01 +0100

    modified primaryyyy dxml y controller

--- GestorFactura/src/main/java/Controlador/PrimaryController.java
+++ GestorFactura/src/main/java/Controlador/PrimaryController.java
@@ -12,6 +12,7 @@ import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class PrimaryController implements Initializable{
@@ -28,7 +29,14 @@ public class PrimaryController implements Initializable{
     @FXML
     private Button button_nuevo;
     
+    @FXML
+    private AnchorPane pane_abrir;
 
+    @FXML
+    private AnchorPane pane_info;
+
+    @FXML
+    private AnchorPane pane_nuevo;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -36,15 +44,15 @@ public class PrimaryController implements Initializable{
     }
     
     @FXML
-    private void switchToSecondary() throws IOException {
+    private void onCrearEmpresa(ActionEvent event) throws IOException {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/davinci/gestorfactura/secondary.fxml"));
             Parent root = loader.load();
 
 
-            SecondaryController secController = loader.getController();
+            //SecondaryController secController = loader.getController();
 
-            secController.irATab(secController.getTabEmpresa());
+            //secController.irATab(secController.getTabEmpresa());
 
             Stage stage = new Stage();
             stage.setScene(new Scene(root));
@@ -55,6 +63,28 @@ public class PrimaryController implements Initializable{
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    
+    @FXML
+    void onPaneAbrirEmpresa(ActionEvent event) {
+        pane_info.setVisible(false);
+        pane_nuevo.setVisible(false);
+        pane_abrir.setVisible(true);
+    }
+
+    @FXML
+    void onPaneInformacion(ActionEvent event) {
+        pane_info.setVisible(true);
+        pane_nuevo.setVisible(false);
+        pane_abrir.setVisible(false);
+    }
+    
+    @FXML
+    void onPaneNuevaEmpresa(ActionEvent event) {
+        pane_info.setVisible(false);
+        pane_nuevo.setVisible(true);
+        pane_abrir.setVisible(false);
     }
     
 
