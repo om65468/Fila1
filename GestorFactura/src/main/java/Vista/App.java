f9a697b64b6931e32e57c095fd8723815ad6145f
Omar <omarquinte343@gmail.com>
2025-12-04 20:50:00 +0100

    proveedor, cliente, producto con css (lo de app no es nada)

--- GestorFactura/src/main/java/Vista/App.java
+++ GestorFactura/src/main/java/Vista/App.java
@@ -11,8 +11,6 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    
-    private static Object controller;
 
     @Override
     public void start(Stage stage) throws IOException {
@@ -25,22 +23,12 @@ public class App extends Application {
     }
 
     public static void setRoot(String fxml) throws IOException {
-        FXMLLoader loader = new FXMLLoader(App.class.getResource("/davinci/gestorfactura/ventana_principal.fxml"));
-        Parent root = loader.load();
-        scene.setRoot(root);
+        scene.setRoot(loadFXML(fxml));
     }
-
-
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/davinci/gestorfactura/" + fxml + ".fxml"));
-        Parent root = fxmlLoader.load();
-    controller = fxmlLoader.getController();
-    return root;
-    }
-    
-    public static Object getController() {
-        return controller;
+        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
